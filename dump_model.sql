92d680f192d363fc5667fcd0a76aa34432aa36f1
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2023-03-06 23:50:11 -0300

    Iniciando Projeto

---
+++ dump_model.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(55) NOT NULL,
+    email VARCHAR(55) NOT NULL UNIQUE,
+    password VARCHAR(55) NOT NULL,
+    "imageUrl" TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL,
+    "userId" INTEGER NOT NULL REFERENCES users(id),
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    "userId" INTEGER NOT NULL REFERENCES users(id),
+    "postId" INTEGER NOT NULL REFERENCES posts(id),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE trends (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(55) NOT NULL UNIQUE,
+    "userId" INTEGER NOT NULL REFERENCES users(id),
+    count INTEGER NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
