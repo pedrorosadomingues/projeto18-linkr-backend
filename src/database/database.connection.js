460e35800f5f1a05edae57b522246d6bdd107aaf
luiza-paloschi <luizapaloschi@gmail.com>
2023-03-10 01:03:55 -0300

    fix: database

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -6,7 +6,7 @@ const { Pool } = pg;
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-  ...(process.env.MODE === "production" && {
+  ...(process.env.NODE_ENV === "production" && {
     ssl: {
       rejectUnauthorized: false,
     },
